964268aaecd63168841b8e76eac810fcc37015da
Vidya Sagar Gupta <sagarvns2003@gmail.com>
2019-09-21 22:15:01 +0530

    JUnit class added

--- src/test/java/com/vidya/MultiCastingFlowTest.java
+++ src/test/java/com/vidya/MultiCastingFlowTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PollingFlowTest {
+public class MultiCastingFlowTest {
 
 	@Test
 	public void contextLoads() {
